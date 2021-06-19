8ab58f4c81e918f60e81fca3958710065b0006b8
pieterhelsen <doccie@gmail.com>
2021-06-19 19:54:49 +0200

    Add 'full' option

--- hdd-serial.sh
+++ hdd-serial.sh
@@ -1,1 +1,6 @@
-udevadm info --query=all --name=$1 | grep ID_SERIAL
+if [ ! $2 -eq 'full' ]
+then
+	udevadm info --query=all --name=$1
+else
+	udevadm info --query=all --name=$1 | grep ID_SERIAL
+fi
